5bf37f763f99c4688df0477166c957329bdf9155
armanbayona <arman.bayona@boom.camp>
2019-08-02 16:18:46 +0800

    created dummy address book ui

--- src/components/AddressBook.js
+++ src/components/AddressBook.js
@@ -1,22 +1,34 @@
-import React from 'react';
-
-import { fade, makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import Badge from '@material-ui/core/Badge';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import MoreIcon from '@material-ui/icons/MoreVert';
-
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import React, { useState } from 'react';
+import clsx from 'clsx';
+import { 
+  AppBar, 
+  Toolbar, 
+  Container, 
+  Grid, 
+  Box, 
+  Button,
+  Typography,
+  IconButton,
+  Avatar,
+  Card,
+  CardHeader,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Collapse,
+} from '@material-ui/core';
+import { 
+  Menu, 
+  AccountCircle,
+  MoreVert,
+  ExpandMore, 
+} from '@material-ui/icons';
+import { 
+  makeStyles, 
+  MuiThemeProvider, 
+  createMuiTheme 
+} from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
   palette: {
@@ -33,216 +45,85 @@ const theme = createMuiTheme({
 });
 
 const useStyles = makeStyles(theme => ({
-  grow: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.grey[700],
     },
-  },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
+    expand: {
+      transform: 'rotate(0deg)',
+      marginLeft: 'auto',
+      transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+      }),
     },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(3),
-      width: 'auto',
+    expandOpen: {
+      transform: 'rotate(180deg)',
     },
-  },
-  searchIcon: {
-    width: theme.spacing(7),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 7),
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: 200,
-    },
-  },
-  sectionDesktop: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
-    },
-  },
-  sectionMobile: {
-    display: 'flex',
-    [theme.breakpoints.up('md')]: {
-      display: 'none',
-    },
-  },
+  }
 }));
 
-function AddressBook() {
-	const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
-
-  const isMenuOpen = Boolean(anchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-  function handleProfileMenuOpen(event) {
-    setAnchorEl(event.currentTarget);
-  }
-
-  function handleMobileMenuClose() {
-    setMobileMoreAnchorEl(null);
-  }
-
-  function handleMenuClose() {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  }
-
-  function handleMobileMenuOpen(event) {
-    setMobileMoreAnchorEl(event.currentTarget);
-  }
-
-  const menuId = 'primary-search-account-menu';
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-    </Menu>
-  );
-
-  const mobileMenuId = 'primary-search-account-menu-mobile';
-  const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
-    >
-      <MenuItem>
-        <IconButton aria-label="show 4 new mails" color="inherit">
-          <Badge badgeContent={4} color="secondary">
-            <MailIcon />
-          </Badge>
-        </IconButton>
-        <p>Messages</p>
-      </MenuItem>
-      <MenuItem>
-        <IconButton aria-label="show 11 new notifications" color="inherit">
-          <Badge badgeContent={11} color="secondary">
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
-        <p>Notifications</p>
-      </MenuItem>
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <IconButton
-          aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
-          color="inherit"
-        >
-          <AccountCircle />
-        </IconButton>
-        <p>Profile</p>
-      </MenuItem>
-    </Menu>
-  );
+function Navbar(props) {
+  const classes = useStyles();
 
   return (
-  	<MuiThemeProvider theme = { theme }>
-    <div className={classes.grow}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="open drawer"
-          >
-            <MenuIcon />
-          </IconButton>
-          
-          <Typography className={classes.title} variant="h6" noWrap>
-          	AddressBook
-          </Typography>
-
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase
-              placeholder="Search…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </div>
-          <div className={classes.grow} />
-
-          <div className={classes.sectionDesktop}>
-            <IconButton
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
-              color="inherit"
-            >
+    <MuiThemeProvider theme = { theme }>
+      <AppBar position="sticky">
+        <Container maxWidth="md">
+          <Toolbar variant="regular">
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <Menu />
+            </IconButton>
+            <Box my={1}>
+              <Typography variant="h6" color="inherit">
+                AddressBook
+              </Typography>
+            </Box>
+            <IconButton edge="end" style={{marginLeft: 'auto'}} color="inherit" aria-label="menu">
               <AccountCircle />
             </IconButton>
-          </div>
-
-          <div className={classes.sectionMobile}>
-            <IconButton
-              aria-label="show more"
-              aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="inherit"
-            >
-              <MoreIcon />
-            </IconButton>
-          </div>
-
-        </Toolbar>
+          </Toolbar>
+        </Container>
       </AppBar>
-      {renderMobileMenu}
-      {renderMenu}
-    </div>
-    </MuiThemeProvider>
+      <Container maxWidth="md">
+        <Box p={2}>
+          {Contact('Arman', 'Bayona', 67645745648)}
+        </Box>
+      </Container>
+    </MuiThemeProvider> 
   );
 }
 
+function Contact(firstname, lastname, number) {
+  const classes = useStyles();
+  const [expanded, setExpanded] = useState(false);
 
+  function handleExpandClick() {
+    setExpanded(!expanded);
+  }
 
-export default AddressBook;
+  return (
+    <Card>
+      <CardHeader avatar={<Avatar aria-label="recipe">R</Avatar>}
+        action={
+          <IconButton 
+            className={clsx(classes.expand, {[classes.expandOpen]: expanded,})}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMore />
+          </IconButton>
+        }
+        title={`${firstname} ${lastname}`}
+        subheader={`${number}`}
+      />
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Details</Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
+
+export default Navbar;
